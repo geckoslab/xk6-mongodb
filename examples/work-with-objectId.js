5656d16c6684f2a2aa85ef561a2396f1f9c16ab2
Dat Tran <dattranit95@gmail.com>
2024-07-29 11:04:46 +0700

    feat: add new ObjectID methods

--- examples/work-with-objectId.js
+++ examples/work-with-objectId.js
@@ -2,13 +2,10 @@ import xk6_mongodb from 'k6/x/mongodb'
 
 const connection = xk6_mongodb.connect('mongodb://localhost:27017')
 
-let doc = {
+const doc = {
   name: 'k6',
-  ref_id: {
-    $oid: '66a6f37b369b0796f6f7f840'
-  }
+  ref_id: xk6_mongodb.newId(),
 }
-doc = connection.transformDoc(doc)
 
 export default function () {
   connection.insert('k6', 'k6', doc)
--- mongodb.go
+++ mongodb.go
@@ -2,13 +2,11 @@ package xk6_mongodb
 
 import (
 	"context"
-	"log"
-	"reflect"
-
 	k6Modules "go.k6.io/k6/js/modules"
-	mongoPrimitives "go.mongodb.org/mongo-driver/bson/primitive"
+	mongoPrimitive "go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	mongoOpts "go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 func init() {
@@ -21,7 +19,7 @@ type Connection struct {
 	Client *mongo.Client
 }
 
-func (m *MongoDb) Connect(url string) *Connection {
+func (*MongoDb) Connect(url string) *Connection {
 	connectionOpts := mongoOpts.Client().ApplyURI(url)
 	client, err := mongo.Connect(context.Background(), connectionOpts)
 
@@ -34,24 +32,17 @@ func (m *MongoDb) Connect(url string) *Connection {
 	}
 }
 
-func (connection *Connection) TransformDoc(doc interface{}) interface{} {
-	// Transform all $oid (nested) to ObjectID
-	for key, value := range doc.(map[string]interface{}) {
-		if reflect.TypeOf(value).Kind() == reflect.Map {
-			if _, ok := value.(map[string]interface{})["$oid"]; ok {
-				newObjectID, err := mongoPrimitives.ObjectIDFromHex(value.(map[string]interface{})["$oid"].(string))
-				if err != nil {
-					log.Fatalf("Error when converting string to ObjectID: %v", err)
-				}
+func (*MongoDb) NewId() mongoPrimitive.ObjectID {
+	return mongoPrimitive.NewObjectID()
+}
 
-				doc.(map[string]interface{})[key] = newObjectID
-			} else {
-				// TODO: Handle nested object
-			}
-		}
+func (*MongoDb) NewIdFromHex(hex string) mongoPrimitive.ObjectID {
+	objectId, err := mongoPrimitive.ObjectIDFromHex(hex)
+	if err != nil {
+		log.Fatalf("Error when creating ObjectID from hex: %v", err)
 	}
 
-	return doc
+	return objectId
 }
 
 func (connection *Connection) Insert(dbName string, collName string, doc interface{}) error {
